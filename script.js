66a35584b706ffdfeb23a4b820a929e5fef6c832
Michael Haji <michaelh1277@gmail.com>
2023-06-28 01:23:34 -0700

    update index.html and script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 const year = new Date().getFullYear()
 document.getElementById('year').innerHTML = year
-
-document
-  .getElementByClass('signup-image')
-  .addEventListener('click', function () {
-    window.location.href = 'https://www.kidney.org/transplantation/beadonor'
-  })
